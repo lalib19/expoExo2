63ade98ad815781e42f6e3512777c3e2d5daad52
louis alibert <louis.alib19@gmail.com>
2022-05-10 12:24:25 +0200

    second expo exercise

--- App.js
+++ App.js
@@ -4,8 +4,16 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
       <StatusBar style="auto" />
+
+      <View style={styles.container2}>
+      </View>
+      <View style={styles.container3}>
+      </View>
+      <View style={styles.container4}>
+      </View>
+
     </View>
   );
 }
@@ -14,7 +22,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'row',
   },
+  container2: {
+    flex: 3,
+    backgroundColor: "green",
+  },
+  container3: {
+    flex: 2,
+    backgroundColor: 'red',
+  },
+  container4: {
+    flex: 1,
+    backgroundColor: 'blue',
+  },
+
 });
